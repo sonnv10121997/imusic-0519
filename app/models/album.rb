c9560ebff69e1036ab1f16db3b6444179cb7fb8d
sonnv-1936 <nguyen.van.son@sun-asterisk.com>
2019-06-17 11:19:47 +0700

    add admin/albums_controller.rb

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,6 @@
 class Album < ApplicationRecord
+  ALBUM_PARAMS = %i(name year artist_id).freeze
+
   belongs_to :artist
 
   has_many :category_albums
@@ -6,4 +8,12 @@ class Album < ApplicationRecord
   has_many :favourites, as: :favourable
   has_many :categories, through: :category_albums
   has_many :tracks, through: :album_tracks
+
+  delegate :name, to: :artist, prefix: true
+
+  validates :name, length: {maximum: Settings.album.name.max_length},
+    presence: true
+  validates :year, presence: true, numericality: {only_integer: true,
+    greater_than: Settings.album.year.minimum}
+  validates :artist, presence: true
 end
