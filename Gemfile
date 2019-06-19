d276dc5958751dbaf119d0dd468798a717a0bf2b
sonnv-1936 <nguyen.van.son@sun-asterisk.com>
2019-06-19 08:38:44 +0700

    attach cover to category.rb

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) {|repo| "https://github.com/#{repo}.git"}
 
 ruby "2.6.1"
 
+gem "active_storage_validations", "0.7.1"
 gem "bootsnap", "1.4.4", require: false
 gem "bootstrap", "4.3.1"
 gem "config", "1.7.1"
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,12 @@
 class Category < ApplicationRecord
+  CATEGORY_PARAMS = %i(name cover).freeze
+
   has_many :category_albums
   has_many :albums, through: :category_albums
+  has_one_attached :cover
 
   validates :name, presence: true, uniqueness: {case_sensitive: false},
     length: {maximum: Settings.category.name.max_length}
+  validates :cover, attached: true, content_type: Settings.category.cover.file_type,
+    size: {less_than: Settings.category.cover.max_size.megabytes}
 end
