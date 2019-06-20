b22b9a65bc7928ccc603da7baf535e7b860c1c41
sonnv-1936 <nguyen.van.son@sun-asterisk.com>
2019-06-20 11:36:20 +0700

    attach data to track.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,14 @@ module ApplicationHelper
     page_title.empty? ? base_title : page_title << " | " << base_title
   end
 
-  def rescue_image image, css_class
+  def rescue_image image, css_class = nil
     return unless image.attached?
     content_tag :image, nil, src: url_for(image), class: css_class
+  end
+
+  def rescue_audio audio, css_class, id = nil
+    return unless audio.attached?
+    content_tag :audio, nil, src: url_for(audio), controls: true,
+      preload: "auto", class: css_class, id: id
   end
 end
